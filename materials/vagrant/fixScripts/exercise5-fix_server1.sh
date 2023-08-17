2d79b7518a7ec6565d331548c321663d7c0df7fb
Ben-Lavi <lavi.ben@gmail.com>
2023-08-17 18:29:13 +0300

    solve ext4 + ext5

--- materials/vagrant/fixScripts/exercise5-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
+
+ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa
+sudo apt install sshpass
+sshpass -p vagrant ssh-copy-id vagrant@server2
--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
+
+
+ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa
+sudo apt install sshpass
+sshpass -p vagrant ssh-copy-id vagrant@server1
