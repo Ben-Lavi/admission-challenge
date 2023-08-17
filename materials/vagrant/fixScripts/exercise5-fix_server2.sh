c5bde9e443e499dab76044aa6bccaa4df56a1b20
Ben-Lavi <lavi.ben@gmail.com>
2023-08-17 22:58:34 +0300

    solv ext5

--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
 
+sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sudo systemctl restart sshd.service
 
-ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa
-sudo apt install sshpass
-sshpass -p vagrant ssh-copy-id vagrant@server1
+sudo sed -i 's/#   StrictHostKeyChecking ask/    StrictHostKeyChecking no/g' /etc/ssh/ssh_config
+sudo -u vagrant ssh-keygen -q -t rsa -N '' -f /home/vagrant/.ssh/id_rsa
+sudo apt-get update
+sudo apt-get install sshpass
+sshpass -p vagrant ssh-copy-id -i /home/vagrant/.ssh/id_rsa.pub vagrant@192.168.60.10 -f 
